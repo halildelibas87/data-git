59acc6151d8c6fef5b3266689845362a50b13b81
Halil Delibas <halildelibas87@gmail.com>
2026-02-09 22:04:40 +0300

    Fix style issues, should get a 'Good Style' now :pray:

--- today.py
+++ today.py
@@ -1,10 +1,8 @@
-'''
-This is the module docstring
-'''
+"""A module computing buddy pair names for the day"""
 
-
-pass  # YOUR CODE HERE
 def my_name_is():
+    """Return my GitHub username."""
     return "halildelibas87"
 def my_buddy_is():
+    """Return my buddy's GitHub username."""
     return "halildelibas87"
